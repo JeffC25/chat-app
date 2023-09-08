6b766d3efb60ca76ca3b2af09cb24599ad81e2ec
John Burke <john@crc-dot1x-nat-10-239-117-42.bu.edu>
2023-09-08 15:26:53 -0400

    Updated firebase.js, added function for GMAIL sign in

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,4 +1,7 @@
-export const firebaseConfig = {
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
     apiKey: "AIzaSyA2a3Ia3PwV26dT25W2AEYQMR1Z10KV5j0",
     authDomain: "chat-app-b275d.firebaseapp.com",
     projectId: "chat-app-b275d",
@@ -7,3 +10,20 @@ export const firebaseConfig = {
     appId: "1:979674436067:web:e9a528aedc2f141058d0d6",
     measurementId: "G-VP82B81KTM"
 };
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+const provider = new GoogleAuthProvider();
+export function userSignIn() {
+    signInWithPopup(auth, provider)
+        .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        const user = result.user;
+        })
+        .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+    });
+}
