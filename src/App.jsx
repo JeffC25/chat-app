d88baaa4c7e8ef56270b3bf96a9767251c12d2ef
John Burke <john@crc-dot1x-nat-10-239-117-42.bu.edu>
2023-09-08 14:56:38 -0400

    Created utils/firebase.js to contain firebase configurations; initialized App in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "./utils/firebase"
+
+const app = initializeApp(firebaseConfig);
+
 function App() {
+  console.log(app);
   return (
     <>
       Hello World!
---
+++ src/utils/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA2a3Ia3PwV26dT25W2AEYQMR1Z10KV5j0",
+    authDomain: "chat-app-b275d.firebaseapp.com",
+    projectId: "chat-app-b275d",
+    storageBucket: "chat-app-b275d.appspot.com",
+    messagingSenderId: "979674436067",
+    appId: "1:979674436067:web:e9a528aedc2f141058d0d6",
+    measurementId: "G-VP82B81KTM"
+};
