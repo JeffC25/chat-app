7c55575dfd3d553fcfb8bf6a0558a188c40a4e8b
John Burke <john@crc-dot1x-nat-10-239-117-42.bu.edu>
2023-09-08 15:27:40 -0400

    Reverted App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
-import { initializeApp } from "firebase/app";
-import { firebaseConfig } from "./utils/firebase"
-
-const app = initializeApp(firebaseConfig);
-
 function App() {
-  console.log(app);
   return (
     <>
       Hello World!
