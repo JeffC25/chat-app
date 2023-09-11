46cdcd839acb6356e7b81e715430e9839ce6fd74
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-09-10 23:20:23 -0400

    clean up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import * as utils from '../../utils/firebase';
+import * as utils from '../utils/firebase';
 
 const SearchBar = () => {
     return (
@@ -8,16 +8,16 @@ const SearchBar = () => {
                 <svg fill="#c0c0c0" height="24px" width="24px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 488.4 488.4" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M0,203.25c0,112.1,91.2,203.2,203.2,203.2c51.6,0,98.8-19.4,134.7-51.2l129.5,129.5c2.4,2.4,5.5,3.6,8.7,3.6 s6.3-1.2,8.7-3.6c4.8-4.8,4.8-12.5,0-17.3l-129.6-129.5c31.8-35.9,51.2-83,51.2-134.7c0-112.1-91.2-203.2-203.2-203.2 S0,91.15,0,203.25z M381.9,203.25c0,98.5-80.2,178.7-178.7,178.7s-178.7-80.2-178.7-178.7s80.2-178.7,178.7-178.7 S381.9,104.65,381.9,203.25z"></path> </g> </g> </g></svg>
             </button>
         </form>
-    )
-}
+    );
+};
 
 const LoginButton = () => {
     return (
         <button className="h-12 w-20 bg-gray-800 rounded-full text-white self-center" onClick={utils.userSignIn}>
             Login
         </button>
-    )
-}
+    );
+};
 
 const Navbar = () => {
     return (
@@ -30,7 +30,7 @@ const Navbar = () => {
             <div className="flex-grow text-right px-4"> {LoginButton()} </div>
             
         </div>
-    )
-}
+    );
+};
 
 export default Navbar;
