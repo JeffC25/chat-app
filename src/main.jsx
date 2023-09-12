532a39d003a4137a0a842a2cec0b13a7338e1002
jwburke02 <jwburke@bu.edu>
2023-09-12 14:38:11 -0400

    Refactored + restored main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Authentication } from './utils/Authentication';
-import { addUserByUID, getEmailByUID, sendAMessage, getListOfMessages, getListOfFriends, getMostRecentMessage } from "./utils/database";
-
-console.log(await getListOfFriends("jwburke2002@gmail.com"))
-console.log(await getMostRecentMessage("jwburke2002@gmail.com", "jwburke@bu.edu"))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Authentication>
