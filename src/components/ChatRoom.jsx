93cea71e71b7bc2312cbe598cd65113dd198360a
jwburke02 <jwburke@bu.edu>
2023-09-12 14:37:47 -0400

    Initial DB operations completed @TODO: Testing database.js + incorporate into UI

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,12 +1,12 @@
-import { addUserByUID } from "../utils/database";
+import { addUserByUID, getEmailByUID, sendAMessage } from "../utils/database";
 
 const Message = (content, fromUser) => {
     // TODO: Check if message is from current user
     return (
-        
+
         <div className={`flex items-center ${fromUser ? "flex-row-reverse" : "flex-row"} `}>
             <div className="w-12 h-12 rounded-full bg-neutral-200">
-                <img src=""/>
+                <img src="" />
             </div>
             <div className="w-fit bg-neutral-200 m-2 rounded-full">
                 <div className="m-4 text-neutral-700">
@@ -29,27 +29,29 @@ const Converstation = (messages) => {
 const Input = () => {
     return (
         <form className="h-12 w-2/3 flex self-center">
-            <input type="text" placeholder="Messsage" className="h-full w-full bg-neutral-200 rounded-l-full my-auto pl-4 outline-none"/>
+            <input type="text" placeholder="Messsage" className="h-full w-full bg-neutral-200 rounded-l-full my-auto pl-4 outline-none" />
             <button type="submit" className="h-full w-20 rounded-r-full bg-neutral-300 ">Send</button>
         </form>
     );
 };
 
 const Dock = () => {
-    return(
+    return (
         <div className="h-20 w-full bg-transparent fixed bottom-0 flex justify-center items-center">
             {Input()}
         </div>
     );
 };
 
-const ChatRoom = ({user}) => {
+const ChatRoom = ({ user }) => {
     // TODO: Fetch messages from database
     const messages = [Message("Hello", true), Message("World", false)];
-    if(user.email) {
+    if (user.email) {
+        //this piece of code means that whenever the ChatRoom is opened
+        //(which means a user is signed in) the app will ensure the user is in the DB
         addUserByUID(user.uid, user.email)
     }
-    return(
+    return (
         <div className="flex flex-col h-full">
             {Converstation(messages)}
             {Dock()}
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Authentication } from './utils/Authentication';
-import { getListOfUsers } from './utils/database.js';
+import { addUserByUID, getEmailByUID, sendAMessage, getListOfMessages, getListOfFriends, getMostRecentMessage } from "./utils/database";
 
-const emailList = await getListOfUsers();
-console.log(emailList);
+console.log(await getListOfFriends("jwburke2002@gmail.com"))
+console.log(await getMostRecentMessage("jwburke2002@gmail.com", "jwburke@bu.edu"))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Authentication>
