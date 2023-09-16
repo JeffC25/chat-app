8d186075e2d8af4e5598705204292502c7d718bb
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-09-16 12:58:50 -0400

    layout for search results page

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,8 +1,27 @@
+import { Link } from "react-router-dom";
 import Layout from "./Layout"
+
+const userCard = () => {
+    const name = "John Doe";
+    return (
+        <Link to="/" className="flex w-1/2 h-24 bg-[rgba(0,0,0,0.25)] hover:bg-[rgba(0,0,0,0.5)] p-2 rounded-full mx-auto my-2 items-center" >
+            <img src="" alt="" className=" aspect-square w-20 h-20 object-cover rounded-full bg-neutral-400"/>
+            <div className=" pl-2 h-full align-middle items-center flex overflow-clip text-ellipsis text-neutral-200 text-xl">
+                {name}
+            </div>
+        </Link>
+    )
+}
 
 const SearchResults = () => {
     return (
-        <Layout></Layout>
+        <Layout>
+            <div className="h-screen w-screen flex flex-col fixed justify-center ">
+                <div className="overflow-y-scroll w-full mx-auto">
+                    {Array(10).fill(userCard())}
+                </div>
+            </div>
+        </Layout>
     )
 }
 
