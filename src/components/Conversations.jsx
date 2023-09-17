0da0c2234b045fdc0bbeacf9a61ee8b8081cb83e
John Burke <john@nat-wireless-guest-reg-153-51.bu.edu>
2023-09-17 17:04:39 -0400

    Updated conversations user card

--- src/components/Conversations.jsx
+++ src/components/Conversations.jsx
@@ -5,11 +5,11 @@ import { useState } from "react";
 import { getAuth } from "firebase/auth";
 import { onAuthStateChanged } from "firebase/auth";
 
-const userCard = () => {
-    const name = "John Doe";
+const userCard = (user) => {
+    const name = user.name;
     return (
         <Link to="/" className="flex w-1/2 h-24 bg-[rgba(0,0,0,0.25)] hover:bg-[rgba(0,0,0,0.5)] p-2 rounded-full mx-auto my-2 items-center" >
-            <img src="" alt="" className=" aspect-square w-20 h-20 object-cover rounded-full bg-transparent p-1"/>
+            <img src={user.photo} alt="" className=" aspect-square w-20 h-20 object-cover rounded-full bg-transparent p-1"/>
             <div className=" pl-2 h-full align-middle items-center flex overflow-clip text-ellipsis text-neutral-200 text-xl">
                 {name}
             </div>
@@ -38,7 +38,8 @@ const Conversations = ({user}) => {
         <Layout>
             <div className="h-full w-screen flex flex-col absolute justify-center -mt-20 -z-10">
                 <div className="overflow-y-scroll w-full mx-auto h-3/4 ">
-                    {userFriendList.fill(userCard())}
+                    {userFriendList.map((user) => {
+                        return userCard(user)})};
                 </div>
             </div>
         </Layout>
