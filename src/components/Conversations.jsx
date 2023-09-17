1b5e4b01294ee2f788d08b096727c5b2a498adfc
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-09-17 16:33:21 -0400

    add conversation page

---
+++ src/components/Conversations.jsx
@@ -0,0 +1,34 @@
+import { Link, useLocation } from "react-router-dom";
+import Layout from "./Layout"
+
+const userCard = () => {
+    const name = "John Doe";
+    return (
+        <Link to="/" className="flex w-1/2 h-24 bg-[rgba(0,0,0,0.25)] hover:bg-[rgba(0,0,0,0.5)] p-2 rounded-full mx-auto my-2 items-center" >
+            <img src="" alt="" className=" aspect-square w-20 h-20 object-cover rounded-full bg-transparent p-1"/>
+            <div className=" pl-2 h-full align-middle items-center flex overflow-clip text-ellipsis text-neutral-200 text-xl">
+                {name}
+            </div>
+        </Link>
+    )
+}
+
+const Conversations = ({user}) => {
+    // const location = useLocation();
+    // // const path = location.pathname;
+    // const params = new URLSearchParams(location.search);
+
+    console.log("conversations for" + user)
+
+    return (
+        <Layout>
+            <div className="h-full w-screen flex flex-col absolute justify-center -mt-20 -z-10">
+                <div className="overflow-y-scroll w-full mx-auto h-3/4 ">
+                    {Array(10).fill(userCard())}
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export default Conversations;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 // import { useNavigate } from 'react-router';
-import { createSearchParams, useNavigate, useLocation } from 'react-router-dom';
+import { createSearchParams, useNavigate, useLocation, Link } from 'react-router-dom';
 import { getAuth, signOut } from "firebase/auth";
 import { useState } from 'react';
 import '../utils/firebase';
@@ -24,12 +24,23 @@ const SearchBar = () => {
             onChange={(e) => setQuery(e.target.value)}
             className="h-12 w-2/3 flex self-center shadow-lg rounded-full z-50">
             <input type="text" placeholder="Add User by Email" className="h-full w-full bg-neutral-200 text-neutral-500 rounded-l-full my-auto pl-4 outline-none"/>
-            <button onClick={(event) => {handleSubmit(event)}} type="submit" className="h-full w-20 rounded-r-full bg-neutral-300 pl-4">
-                +
+            <button onClick={(event) => {handleSubmit(event)}} type="submit" className="h-full w-20 rounded-r-full bg-neutral-300 text-cyan-600">
+                Add
             </button>
         </form>
     );
 };
+
+const ConvoButton = () => {
+    return (
+        <div className="h-12 w-20 bg-neutral-200 hover:bg-neutral-400 rounded-full flex items-center align-middle shadow-lg">
+            <Link to="/conversations" className="h-full w-full self-center text-cyan-600 flex items-center justify-center">
+                Chats
+            </Link>
+
+        </div>
+    );
+}
 
 const LogoutButton = () => {
     const navigate = useNavigate();
@@ -52,7 +63,7 @@ const Navbar = () => {
     return (
         <div className="h-20 w-full bg-transparent backdrop-opacity-5 flex justify-between items-center">
 
-            <div className="flex-grow"></div>
+            <div className="flex-grow text-right px-4"> {ConvoButton()} </div>
 
             <div className="flex-grow flex justify-center"> {SearchBar()} </div>
 
