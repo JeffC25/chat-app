3bb71da077ff372a889c2b07ba281d0784293b5a
John Burke <john@Johns-MacBook-Pro.local>
2023-09-17 13:52:00 -0400

    Updates

--- src/utils/database.js
+++ src/utils/database.js
@@ -42,10 +42,75 @@ export async function getListOfUsers() {
   return emailList;
 }
 
+//creates and stores a new room to the database and stores the room id in the
+// /users/uid/rooms object
+export async function createRoom(user_email, other_email) {
+  //first we create the room
+  const db = getDatabase();
+  const roomKey = dbRef.key();
+
+  const memberStruct = [getUIDByEmail(user_email), getUIDByEmail(other_email)];
+  
+  var chatRoomData = {
+    id: roomKey,
+    messages: {},
+    users: memberStruct
+  }
+
+  push(ref(db, 'rooms/'), chatRoomData);
+
+  //store room data
+  var roomKeyData = {
+    id: roomKey,
+    roomKey: roomKey
+  }
+  push(ref(db, 'users/' + memberStruct[0] + '/rooms'), roomKeyData);
+  push(ref(db, 'users/' + memberStruct[1] + '/rooms'), roomKeyData);
+}
+
+// get all rooms of a user
+export async function getUsersRooms(user_email) {
+  const uid = getUIDByEmail(user_email);
+  const dbRef = ref(getDatabase());
+  const snapshot = await get(child(dbRef, 'users/' + uid + '/rooms'))
+  const value = snapshot.val();
+  var roomList = [];
+  for (const key in value) { //because of auto keys this should go chronologically
+    roomList.push(
+      key
+    )
+  }
+  return roomList;
+}
+
+// send message
+export async function sendMessage(sender_email, room_id, message) {
+  var msgData = {
+    sender: sender_email,
+    message: message
+  }
+  push(ref(db, 'rooms/' + room_id + '/messages'), msgData);
+}
+// get all messages from room
+export async function getRoomMessages(room_id) {
+  const dbRef = ref(getDatabase());
+  const snapshot = await get(child(dbRef, 'rooms/' + room_id + '/messages'))
+  const value = snapshot.val();
+  var msgList = [];
+  for (const key in value) { //because of auto keys this should go chronologically
+    msgList.push({
+      sender: value[key].sender,
+      message: value[key].message
+    })
+  }
+  return msgList;
+}
+
 //this is an async function that will create a chat between two users
 //it takes in (all string) a sender email, receiver email, and initial message
 // it updates the database, also good for just sending a chat
-export async function sendAMessage(sender_email, receiver_email, message) {
+//now not necessary 
+/* export async function sendAMessage(sender_email, receiver_email, message) {
   const db = getDatabase();
   const sender_uid = await getUIDByEmail(sender_email);
   const receiver_uid = await getUIDByEmail(receiver_email);
@@ -55,11 +120,13 @@ export async function sendAMessage(sender_email, receiver_email, message) {
   push(ref(db, 'messages/' + sender_uid + '/' + receiver_uid), payload);
   payload["type"] = "received"
   push(ref(db, 'messages/' + receiver_uid + '/' + sender_uid), payload);
-}
+} */
 
 
 //this function takes in a user email + someone they are chatting with and will return
 //a list chronological chats of the form {type: str, message: str} or an empty list
+// deprecated?
+/*
 export async function getListOfMessages(user_email, recipient_email) {
   const user_uid = await getUIDByEmail(user_email);
   const recipient_uid = await getUIDByEmail(recipient_email);
@@ -74,10 +141,12 @@ export async function getListOfMessages(user_email, recipient_email) {
     })
   }
   return msgList;
-}
+}*/
 
+
+//deprecated
 // this function returns every user email that the given account email has open chats with
-export async function getListOfFriends(user_email) {
+/* export async function getListOfFriends(user_email) {
   const user_uid = await getUIDByEmail(user_email);
   const dbRef = ref(getDatabase());
   const snapshot = await get(child(dbRef, 'messages/' + user_uid))
@@ -89,11 +158,13 @@ export async function getListOfFriends(user_email) {
     )
   }
   return emailList;
-}
+} */
 
 //this function will return the top message that the given pair last had in conversation
+//deprecated
+/*
 export async function getMostRecentMessage(user_email, recipient_email) {
   const msgList = await getListOfMessages(user_email, recipient_email)
   const message = msgList[msgList.length - 1];
   return message;
-}
+}*/
