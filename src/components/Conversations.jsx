2d58b89245a908d8aad4dbdbd70819140819220a
John Burke <john@nat-wireless-guest-reg-153-51.bu.edu>
2023-09-17 17:51:21 -0400

    Initial commit for working ID Gen

--- src/components/Conversations.jsx
+++ src/components/Conversations.jsx
@@ -1,20 +1,25 @@
 import { Link } from "react-router-dom";
 import Layout from "./Layout"
-import { getUsersFriends } from "../utils/database";
+import { generateRoomIDFromEmails, getUsersFriends } from "../utils/database";
 import { useState } from "react";
 import { getAuth } from "firebase/auth";
 import { onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom"
 
-const userCard = (user) => {
+const userCard = (user, email, navigate) => {
     const name = user.name;
+    async function handleClick() {
+        const roomID = await generateRoomIDFromEmails(email, user.email);
+        navigate("/chat/" + roomID);
+    }
 
     return (
-        <Link to="/chat/global" className="flex w-1/2 h-24 bg-[rgba(0,0,0,0.25)] hover:bg-[rgba(0,0,0,0.5)] p-2 rounded-full mx-auto my-2 items-center" >
+        <div onClick={() => handleClick()} className="flex w-1/2 h-24 bg-[rgba(0,0,0,0.25)] hover:bg-[rgba(0,0,0,0.5)] p-2 rounded-full mx-auto my-2 items-center" >
             <img src={user.photo} alt="" className=" aspect-square w-20 h-20 object-cover rounded-full bg-transparent p-1"/>
             <div className=" pl-2 h-full align-middle items-center flex overflow-clip text-ellipsis text-neutral-200 text-xl">
                 {name}
             </div>
-        </Link>
+        </div>
     )
 }
 
@@ -22,6 +27,7 @@ const Conversations = ({user}) => {
     // const location = useLocation();
     // // const path = location.pathname;
     // const params = new URLSearchParams(location.search);
+    const navigate = useNavigate();
     const [userFriendList, setUserFriendList] = useState([]);
     const auth = getAuth();
     onAuthStateChanged(auth, (user) => {
@@ -40,7 +46,7 @@ const Conversations = ({user}) => {
             <div className="h-full w-screen flex flex-col absolute justify-center -mt-20 -z-10">
                 <div className="overflow-y-scroll w-full mx-auto h-3/4 ">
                     {userFriendList.map((user) => {
-                        return userCard(user)})};
+                        return userCard(user, auth.currentUser.email, navigate)})};
                 </div>
             </div>
         </Layout>
