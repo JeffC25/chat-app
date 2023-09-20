280d821d34d6395fb0e3d1787dc22e485643a20a
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-09-19 23:41:10 -0400

    change layout positioning from absolute to fixed -- fix zoomed in laptop screens scrolling background

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 
 const Layout = ({children}) => {
     return (
-        <div className="absolute 
+        <div className="fixed 
             bg-gradient-to-t from-blue-600 to-cyan-500
             h-max min-h-screen w-full overflow-auto -z-50">
             <div className="z-50">
